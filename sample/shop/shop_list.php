c1e47ab16049fb51e89b98a1bad4451544e0f4ac
pankun4 <s1542105su@s.chibakoudai.jp>
2017-06-23 15:55:03 +0900

    c1

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -44,18 +44,77 @@ $dbh=null;
 
 print '商品一覧<br /><br />';
 
+<?php
+require_once('../common/common.php');
+?>
+    
+キーワードを選んでください。<br />
+<from method="post"action="">
+種類
+<?php pulldoun_type(); ?>
+サイズ
+<?php pulldoun_size(); ?>
+走行距離
+<?php pulldoun_color(); ?>
+<br />
+<input type="submit" value="value="絞り込み">
+</from>
+
+<?php
+
+//フリーキーワード
+$keyword='';
+if(isset($_POST['keyword'])){
+    $keysord=$_POST['keyword'];
+}
+if($keyword!==''){
+    print $keyword.'が含まれる';
+    print '<br />';
+}
+//固定キーワード
+$type='';
+$size='';
+$color='';
+if(isset($POST['type'])){
+   $type=$_POST['type'];
+   $size=$_POST['size'];
+   $color=$_POST['coler'];
+}
+if($type!==''){
+    print $type.' '.$size.','.$coler.'に一致する商品';
+    print '<br />';
+}
+
 while(true)
 {
 	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+            $type2=$rec{'type'];
+            $size2=$rec['size'];
+            $coler2=$rec['coler'];
 	if($rec==false)
 	{
 		break;
 	}
+        
+            $disp=0;
+        //キーワードが空、または、キーワードが含まれるとき表示
+        if(($keyword===''))&&($type==='')){
+            $disp=1;
+        }
+        else if(($type==='')&&(strpos($rec['name'],$keyword)!==false)){
+            $disp=1;
+        }
+        else if(($keyword==='')&&((strpos($type2,$type)!==false)&&(strpos($size2,&size)!
+                $disp=;1
+            }
+            
+    if($disp===1){
 	print '<a href="shop_product.php?procode='.$rec['code'].'">';
 	print $rec['name'].'---';
 	print $rec['price'].'円';
 	print '</a>';
 	print '<br />';
+    }
 }
 
 print '<br />';
